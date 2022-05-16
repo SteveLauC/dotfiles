ee098142ebddf4bc8add32afda29e309a72ffa38
SteveLauC <stevelauc@outlook.com>
2022-05-16 22:26:03 +0800

    change keybinding of telescope

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -85,3 +85,10 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+
+
+
+-- have a fixed column for the diagnostics to appear in
+-- this removes the jitter when warnings/errors flow in
+vim.opt.signcolumn="yes"
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,12 @@
 -- key binding for telescope
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {})
+
+-- control + f
+vim.api.nvim_set_keymap("n", "<C-f>", "<cmd>Telescope find_files<cr>", {}) 
+
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {})
+
+-- control +b 
+-- you need to press ctrl-b-b to enable this cuz ctrl-b is the prefix key of tmux
+vim.api.nvim_set_keymap("n", "<C-b>", "<cmd>Telescope buffers<cr>", {})    
+
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {})
