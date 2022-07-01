28543d7bf424cc5f8073f721fa2f20b173e64460
SteveLauC <stevelauc@outlook.com>
2022-07-01 21:11:26 +0800

    use .zprofile instead of .profile

--- nvim/lua/normal.lua
+++ nvim/lua/normal.lua
@@ -4,4 +4,3 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.expandtab = true
-vim.cmd("hi Visual term=reverse cterm=reverse guibg=Grey")
---
+++ nvim/lua/plugins/kommentary.lua
@@ -0,0 +1,3 @@
+require('kommentary.config').configure_language("rust", {
+    prefer_single_line_comments = true,
+})
