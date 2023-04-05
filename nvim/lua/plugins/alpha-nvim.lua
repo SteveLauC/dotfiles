9ba1f865904f00bd7bc487dbac79988dbd34dc62
Steve Lau <stevelauc@outlook.com>
2023-04-05 22:06:53 +0800

    nvim: change icons for the dashboard

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -15,13 +15,13 @@ dashboard_cfg.section.header.val = {
 
 -- set menu
 dashboard_cfg.section.buttons.val = {
-  dashboard_cfg.button( "n", "  New file" , ":ene <BAR> startinsert <CR>"),
-  dashboard_cfg.button( "f", "  Find file", ":Telescope find_files<CR>"),
+  dashboard_cfg.button( "n", "  New file" , ":ene <BAR> startinsert <CR>"),
+  dashboard_cfg.button( "f", "  Find file", ":Telescope find_files<CR>"),
   dashboard_cfg.button( "t", "  Find Text", ":Telescope live_grep<CR>"),
   dashboard_cfg.button( "r", "  Recent", ":Telescope oldfiles<CR>"),
   dashboard_cfg.button( "q", "  Quit", ":qa<CR>"),
-}
 
+}
 
 -- have no idea what is this, but it seems necessary, right?
 local handle = io.popen('fortune')
@@ -30,6 +30,5 @@ handle:close()
 dashboard_cfg.section.footer.val = fortune
 dashboard_cfg.config.opts.noautocmd = true
 vim.cmd[[autocmd User AlphaReady echo 'ready']]
-
 
 alpha.setup(dashboard_cfg.config)
