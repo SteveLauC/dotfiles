d30adafde0eeaae879bd557a6aec37c7bf7c5255
Steve Lau <stevelauc@outlook.com>
2023-04-05 22:07:22 +0800

    nvim: change diagonstic icons & make trouble.nvim use them

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -45,6 +45,19 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- lsp diagnosic icons
+local signs = {
+    Error = " ",
+    Warn= " ",
+    Hint = " ",
+    Info= " "
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
+
 
 -- have a fixed column for the diagnostics to appear in
 -- this removes the jitter when warnings/errors flow in
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,1 +1,16 @@
+require('trouble').setup {
+    icons = true,
+
+    action_keys = {
+      close = "q", 
+      cancel = "<esc>",
+      refresh = "r",
+    },
+
+    -- Use the icons set in the LSP settings
+    -- See `lsp.lua` for more.
+    use_diagnostic_signs = true
+}
+
+-- key map
 vim.keymap.set('n', '<M-6>', "<cmd>TroubleToggle<CR>")
