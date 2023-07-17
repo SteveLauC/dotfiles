297a8ebbab81578a85e5b634b70358df2dad993e
Steve Lau <stevelauc@outlook.com>
2023-07-17 16:48:17 +0800

    nvim: border for floating window

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -77,3 +77,10 @@ end
 -- this removes the jitter when warnings/errors flow in
 vim.opt.signcolumn="yes"
 
+-- border for floating window
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or "rounded"
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
