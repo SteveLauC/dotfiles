efaca945991fac9ddf9b188ca9e3cdfe874893cb
Steve Lau <stevelauc@outlook.com>
2023-08-26 14:56:21 +0800

    nvim: update icons in alpha-nvim

--- nvim/lua/plugin/alpha-nvim.lua
+++ nvim/lua/plugin/alpha-nvim.lua
@@ -19,11 +19,16 @@ return {
 
 			-- set menu
 			dashboard_cfg.section.buttons.val = {
-				dashboard_cfg.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
-				dashboard_cfg.button("f", "  Find file", ":Telescope find_files<CR>"),
-				dashboard_cfg.button("t", "  Find Text", ":Telescope live_grep<CR>"),
-				dashboard_cfg.button("r", "  Recent", ":Telescope oldfiles<CR>"),
-				dashboard_cfg.button("q", "  Quit", ":qa<CR>"),
+                -- nf-oct-file_added
+				dashboard_cfg.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+                -- nf-md-file_search_outline
+				dashboard_cfg.button("f", "󰱽  Find file", ":Telescope find_files<CR>"),
+                -- nf-md-text_search
+				dashboard_cfg.button("t", "󱎸  Find Text", ":Telescope live_grep<CR>"),
+                -- nf-md-file_clock_outline
+				dashboard_cfg.button("r", "󱋢  Recent", ":Telescope oldfiles<CR>"),
+                -- nf-md-close_circle_outline
+				dashboard_cfg.button("q", "󰅚  Quit", ":qa<CR>"),
 			}
 
 			alpha.setup(dashboard_cfg.config)
